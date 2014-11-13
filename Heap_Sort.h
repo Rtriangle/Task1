d060b7880e3f42f20bdb956dc749bf9b4943b56c
Daniel Korbut <korbut.daniel@gmail.com>
2014-11-13 23:28:32 +0300

    Edit Heap Sort

--- Heap_Sort.h
+++ Heap_Sort.h
@@ -1,13 +1,8 @@
 template <class T>
-void Heap_Sort(T Iter_begin, T Itert_end)
+void Heap_Sort(T Iter_begin, T Iter_end)
 {
 	std::make_heap(Iter_begin,Iter_end);
+	auto Itmoveend = Iter_end;
 	for(int i = 0; i < Iter_end-Iter_begin; i++)
-	{
-		std::pop_heap(begin, end - i);
-        std::iter_swap(begin, end - i - 1);
-    }
-    auto Swap_to = *Iter_begin;
-    std::copy(Iter_begin + 1, Iter_end, Iter_begin);
-    *(Iter_end - 1) = Swap_to;
+		std::pop_heap(Iter_begin, Itmoveend--);
 }
--- main.cpp
+++ main.cpp
@@ -11,51 +11,28 @@
 std::vector <int> V;
 
 template <class T>
-void Insertion_Sort_Copy_For(T Iter_begin, T Iter_end)
+void Heap_Sort(T Iter_begin, T Iter_end)
 {
-	T Iter_move;
-	if(Iter_begin != Iter_end)
-		Iter_move = Iter_begin + 1;
-	T Iter_value;
-	T Iter_fix;
-	bool Doit = true;
-	auto Tmp = *(Iter_begin);
-	while(Iter_move != Iter_end)
-	{
-		Iter_value = Iter_move;
-		Doit = true;
-		while(Iter_value > Iter_begin && Doit)
-		{
-			if(!(*Iter_move < *(Iter_value-1)))
-				Doit = false;
-			else
-				Iter_value--;
-		}
-		Tmp = *(Iter_move);
-		if(Iter_value != Iter_move)
-		{
-			for(T ittmp = Iter_value; ittmp >= Iter_move; ittmp--)
-				std::iter_swap(ittmp,(ittmp+1));
-			*(Iter_value) = Tmp;
-		}
-		Iter_move++;
-	}
+	std::make_heap(Iter_begin,Iter_end);
+	auto Itmoveend = Iter_end;
+	for(int i = 0; i < Iter_end-Iter_begin; i++)
+		std::pop_heap(Iter_begin, Itmoveend--);
 }
-
 
 int main()
 {
 	std::random_device rd;
 	std::mt19937 gen(rd());
 	std::uniform_int_distribution<> dist(1,50);
-	for(int i = 0; i < 5; i++)
+	for(int i = 0; i < 8; i++)
 		V.push_back(dist(gen));
 	for(auto it = V.begin(); it != V.end(); it++)
 		std::cout << *it << " ";
 	std::cout << "\n";
 	auto it1 = V.begin();
 	auto it2 = V.end();
-	Insertion_Sort_Copy_For(it1,it2);
+	std::make_heap(V.begin(),V.end());
+	Heap_Sort(it1,it2);
 	for(auto it = V.begin(); it != V.end(); it++)
 		std::cout << *it << " ";
 	system("pause");
